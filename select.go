47dda604ad54b33fdec45935620bb9957c92f0d3
Tyler Gillson <tyler.gillson@gmail.com>
2023-01-26 16:08:36 -0800

    hack

--- select.go
+++ select.go
@@ -386,12 +386,6 @@ func (s *Select) innerRun(cursorPos, scroll int, top rune) (int, string, error) 
 		sb.WriteString("")
 		sb.Flush()
 		rl.Write([]byte(showCursor))
-		rl.Close()
-
-		rl, e := readline.NewEx(c)
-		if e != nil {
-			return 0, "", err
-		}
 		rl.Write([]byte(doLineWrap))
 		rl.Close()
 
@@ -411,6 +405,7 @@ func (s *Select) innerRun(cursorPos, scroll int, top rune) (int, string, error) 
 	}
 
 	rl.Write([]byte(showCursor))
+	rl.Write([]byte(doLineWrap))
 	rl.Close()
 
 	return s.list.Index(), fmt.Sprintf("%v", item), err
